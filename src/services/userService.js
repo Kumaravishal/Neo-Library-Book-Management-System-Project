276fa7c1fdbfdd7d44bccf4bc80798734d7463bb
KUMARA VISHAL Y <727723eucs110@gmail.com>
2025-08-25 17:24:32 +0530

    initial

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "https://neo-library-book-management-system.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
